6d03d4bd1e89a31120f28d40a72d4509370ca8d7
texmx <david@texmx.net>
2016-08-30 15:33:43 -0500

    Updated script to utilize latest names & URLs
    Changed Lync to Skype
    Changed $Credential to a Global variable
    Changed remote PS URL

--- Connect-Office365.ps1
+++ Connect-Office365.ps1
@@ -12,12 +12,15 @@
    
    .PARAMETER ProxyEnabled
         Specifies that IE Proxy settings should be utilized.
+
+   .PARAMETER AzureAD
+        Specifies that a connection to Azure Active Directory should be made.
    
    .PARAMETER Exchange
         Specifies that a connection to Exchange Online should be made.
    
-   .PARAMETER Lync
-        Specifies that a connection to Lync Online should be made.
+   .PARAMETER Skype
+        Specifies that a connection to Skype for Buisness Online should be made.
    
    .PARAMETER SharePoint
         Specifies that a connection to SharePoint Online should be made.
@@ -81,7 +84,7 @@
         NAME: Microsoft.PowerShell_profile.ps1 
         AUTHOR: David Smith
         LASTEDIT: 01/13/2015
-        KEYWORDS: Office365, Exchange Online, Lync Online, SharePoint Online, Azure AD
+        KEYWORDS: Office365, Exchange Online, Skype for Business Online, SharePoint Online, Azure AD
         VERSION: 2.1 (added #Requires features for all required modules)
         The script are provided “AS IS” with no guarantees, no warranties, and they confer no rights.
    
@@ -98,7 +101,7 @@
             http://go.microsoft.com/fwlink/p/?linkid=236297
 
    .LINK
-        Windows PowerShell Module for Lync Online
+        Windows PowerShell Module for Skype for Business Online
             http://www.microsoft.com/en-us/download/details.aspx?id=39366
 
    .LINK
@@ -113,10 +116,13 @@
         [Switch]$ProxyEnabled,
 
         [Parameter(Mandatory=$false)]
+        [Switch]$AzureAD,
+        
+        [Parameter(Mandatory=$false)]
         [Switch]$Exchange,
 
         [Parameter(Mandatory=$false)]
-        [Switch]$Lync,
+        [Switch]$Skype,
 
         [Parameter(Mandatory=$false)]
         [Switch]$SharePoint,
@@ -127,13 +133,9 @@
         [Parameter(Mandatory=$true)]
         [System.Management.Automation.PSCredential]
         [System.Management.Automation.CredentialAttribute()]
-        $Credential
+        $Global:Credential
     )
 
-     
-    Write-Host "Connecting to Azure Active Directory" -ForegroundColor Yellow
-    Import-Module MSOnline
-    Connect-MsolService -Credential $Credential    
     
     if($ProxyEnabled) {
         $MySessionOption = New-PsSessionOption -ProxyAccessType IEConfig -ProxyAuthentication basic
@@ -141,10 +143,16 @@
         $MySessionOption = New-PSSessionOption -ProxyAccessType None -ProxyAuthentication Negotiate
     }
 
+    if($AzureAD) {
+        Write-Host "Connecting to Azure Active Directory" -ForegroundColor Yellow
+        Import-Module MSOnline
+        Connect-MsolService -Credential $Credential 
+    }
+
     if($Exchange) {
         if ((Get-PSSession | ?{$_.ConfigurationName -eq "Microsoft.Exchange"}).State -ne "Opened") {
             Write-Host "Connecting to Exchange Online" -ForegroundColor Yellow
-            $ExchangeSession = New-Pssession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell -Credential $Credential -Authentication Basic -AllowRedirection -sessionOption $MySessionOption
+            $ExchangeSession = New-Pssession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/PowerShell-liveId -Credential $Credential -Authentication Basic -AllowRedirection -sessionOption $MySessionOption
             Import-PSSession $ExchangeSession
         } Else {
             Write-Host "Connection to Exchange Online exists for " -NoNewline -ForegroundColor Yellow
@@ -152,13 +160,13 @@
         }
     }
     
-    if($Lync) {
+    if($Skype) {
         if ((Get-PSSession | ?{$_.ComputerName -like "*.online.lync.com"}).State -ne "Opened") {
-            Write-Host "Connecting to Lync Online" -ForegroundColor Yellow
-            $LyncSession = New-CsOnlineSession -Credential $Credential     -SessionOption $MySessionOption
-            Import-PSSession $LyncSession
+            Write-Host "Connecting to Skype for Business Online" -ForegroundColor Yellow
+            $SkypeSession = New-CsOnlineSession -Credential $Credential     -SessionOption $MySessionOption
+            Import-PSSession $SkypeSession
         } Else {
-            Write-Host "Connection to Lync Online exists for " -NoNewline -ForegroundColor Yellow
+            Write-Host "Connection to Skype for Business Online exists for " -NoNewline -ForegroundColor Yellow
             Write-Host (Get-CSTenant).DisplayName -ForegroundColor White
         }
     }
@@ -191,16 +199,16 @@ function Disconnect-Office365 {
         [Switch]$Exchange,
 
         [Parameter(Mandatory=$false)]
-        [Switch]$Lync,
+        [Switch]$Skype,
 
         [Parameter(Mandatory=$false)]
         [Switch]$SharePoint
     )
     
-    if ($Exchange -or $Lync -or $SharePoint) {$All = $False}
+    if ($Exchange -or $Skype -or $SharePoint) {$All = $False}
 
     if ($All -or $Exchange)   {Write-Host "Disconnecting Exchange Online" -ForegroundColor Yellow; Get-PSSession | ?{$_.ComputerName -like "*outlook.com"} | Remove-PSSession}
-    if ($All -or $Lync)       {Write-Host "Disconnecting Lync Online" -ForegroundColor Yellow; Get-PSSession | ?{$_.ComputerName -like "*lync.com"} | Remove-PSSession}
+    if ($All -or $Skype)      {Write-Host "Disconnecting Skype for Business Online" -ForegroundColor Yellow; Get-PSSession | ?{$_.ComputerName -like "*lync.com"} | Remove-PSSession}
     if ($All -or $SharePoint) {Write-Host "Disconnecting SharePoint Online" -ForegroundColor Yellow;Disconnect-SPOService -ErrorAction SilentlyContinue}
 }
 
